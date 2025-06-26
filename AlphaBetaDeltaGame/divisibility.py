e9ac556a54ae2ce7552b32d5d600455438ed08d0
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-06-26 04:17:20 +0000

    python solution worked

--- AlphaBetaDeltaGame/divisibility.py
+++ AlphaBetaDeltaGame/divisibility.py
@@ -1,2 +1,16 @@
-function divisibilityGame(n):
+def divisibilityGame(n):
     # Write your logic here
+    for i in range(1, n+1):
+        str = ""
+        if i % 2 == 0:
+            str += "Alpha"
+        if i % 3 == 0:
+            str += "Beta"
+        if i % 5 == 0:
+            str += "Gamma"
+        if str:
+            print(str)
+        else:
+            print(i)
+
+divisibilityGame(30)
